021086987b9faedabf59b7f0b37d97e5f94a6474
Priya <muthupalani1986@gmail.com>
2023-09-22 18:18:23 -0300

    localstorage related changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,8 +6,9 @@ import { LoginPayload, LoginResponse } from './interfaces/login';
 import { SessionStorageService } from '../shared/services/storage/session-storage.service';
 import { SESSION_STORAGE } from '../shared/constants/session-storage.constant';
 import { AuthService } from '../shared/services/auth/auth.service';
-import { Subscription } from 'rxjs';
+import { Subscription, count } from 'rxjs';
 import { ToastService } from '../shared/services/toast/toast.service';
+import { LocalStorageService } from '../shared/services/storage/local-storage.service';
 
 @Component({
   selector: 'workflow-login',
@@ -19,17 +20,23 @@ export class LoginComponent implements OnInit, OnDestroy {
   subscriptions: Subscription[] = [];
   loginForm!: FormGroup;
   hide = true;
+  noOfTimes:number=0;
   constructor(private _router: Router,
     private _loginService: LoginService,
     private _sessionStorageService: SessionStorageService,
     private _authService: AuthService,
     private _formBuilder: FormBuilder,
-    private _toastService: ToastService) { }
+    private _toastService: ToastService,
+    private _localStorageService: LocalStorageService) { }
   ngOnInit() {
     this.loginForm = this._formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
     });
+    if (this._localStorageService.getItem('no_of_times_clicked')) {
+      const data = this._localStorageService.getItem('no_of_times_clicked');
+      this.noOfTimes = parseInt(data.count);
+    }
   }
   public onSubmit() {
     if (this.loginForm.valid) {
@@ -66,6 +73,17 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
   get password() {
     return this.loginForm.get('email');
+  }
+  checkStorage() {
+    let value = { "count": 1 };
+    if (this._localStorageService.getItem('no_of_times_clicked')) {
+      const data = this._localStorageService.getItem('no_of_times_clicked');
+      const count = parseInt(data.count) + 1;
+      value = { count }
+    }
+    console.log("value",value);
+    this.noOfTimes = value.count;
+    this._localStorageService.setItem("no_of_times_clicked", value);
   }
   ngOnDestroy() {
     this.subscriptions.forEach((subscription) => subscription.unsubscribe())
